c3a339fdddccfa0e940e845b7146456da1dd71b1
minju412 <ln8847@ajou.ac.kr>
2022-04-06 00:42:00 +0900

    Feat: Add filtering features
    - lang, categoty, sortby, order 기준 필터링

--- src/controllers/results/results.controller.js
+++ src/controllers/results/results.controller.js
@@ -1,16 +1,32 @@
 const db = require("../../models");
 const Result = db.results;
 
-const getResults = (req, res) => {
-    const pageInfo = req.query;
-    const paged = parseInt(pageInfo.paged);
-    // const itemsPerPage = parseInt(pageInfo.itemsPerPage);
-    // const _limit = itemsPerPage;
+// const Sequelize = require("sequelize");
+// const Op = Sequelize.Op;
 
+// pagination with filtering
+const getResults = (req, res) => {
+    // pagination
+    const paged = req.query.paged;
     const _limit = 10;
     const _offset = (paged-1) * _limit; // 1페이지부터 시작
 
-    Result.findAll({limit:_limit, offset:_offset})
+    // filtering
+    const lang = req.query.lang;
+    const cg = req.query.category;
+    const sortby = req.query.sortby;
+    const order = req.query.order; // asc OR desc
+
+    Result
+        .findAll({
+        where: {
+           language: lang,
+            category: cg
+        },
+        order: [[`${sortby}`, `${order}`]],
+        limit:_limit,
+        offset:_offset
+        })
         .then((data) => {
             res.send(data);
         })
@@ -21,6 +37,28 @@ const getResults = (req, res) => {
             });
         });
 };
+
+//// only pagination
+// const getResults = (req, res) => {
+//     const paged = req.query.paged;
+//     const _limit = 10;
+//     const _offset = (paged-1) * _limit; // 1페이지부터 시작
+//
+//     Result
+//         .findAll({
+//             limit:_limit,
+//             offset:_offset
+//         })
+//         .then((data) => {
+//             res.send(data);
+//         })
+//         .catch((err) => {
+//             res.status(500).send({
+//                 message:
+//                     err.message || "Some error occurred while retrieving results.",
+//             });
+//         });
+// };
 
 const count = (req, res) => {
     try {
