0c7b63c65131aada01c818347308b96c980ce6f3
minju412 <ln8847@ajou.ac.kr>
2022-05-07 11:45:40 +0900

    Feat: Add upload feature
    - 분석 결과(json)를 result-db에 insert

--- src/api/search/search.controller.js
+++ src/api/search/search.controller.js
@@ -1,5 +1,4 @@
 const db = require("../../models");
-const User = db.users;
 const Result = db.results;
 
 // 타겟도메인과 유저 id를 받아오는 기능
@@ -14,33 +13,22 @@ const getDomain = async (req, res, next) => {
 };
 
 // 분석 결과(json)를 결과db에 업로드
-// const upload = (req, res) => {
-//     Result.bulkCreate(results) // db insert
-//         .then(() => {
-//             res.status(200).send({
-//                 message: "db import 성공",
-//             });
-//         })
-//         .catch((error) => {
-//             res.status(500).send({
-//                 message: "db import 실패",
-//                 error: error.message,
-//             });
-//         });
-// };
-
-// 타겟도메인만 받아오는 기능
-// const getDomain = (req, res, next) => {
-//     try {
-//         const domain = req.body.domain; // 타겟 도메인
-//         res.status(200).json(domain);
-//     } catch (error) {
-//         console.error(error);
-//         next(error);
-//     }
-// };
+const upload = (req, res) => {
+    Result.bulkCreate(req.body) // db insert
+        .then(() => {
+            res.status(200).send({
+                message: "db import 성공",
+            });
+        })
+        .catch((error) => {
+            res.status(500).send({
+                message: "db import 실패",
+                error: error.message,
+            });
+        });
+};
 
 module.exports = {
     getDomain,
-    // upload
+    upload
 };
