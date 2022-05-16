9b4927f55febe4585404fe9fe85997de4887678b
minju412 <ln8847@ajou.ac.kr>
2022-05-16 21:29:55 +0900

    Feat: Add 프로젝트 수정 endpoint

--- src/api/project/project.controller.js
+++ src/api/project/project.controller.js
@@ -11,12 +11,37 @@ const createProject = async (req, res) => {
             user_id: req.id, // 해당 프로젝트를 생성한 사용자 id
         });
         // await project.addUser(req.id);
-        res.json({ ProjectId: project.id, UserId: req.id });
+        res.status(200).json({ ProjectId: project.id, UserId: req.id });
     } catch(error){
         console.log(error);
         res.status(500).send({
             message: "프로젝트 생성 실패",
         });
+    }
+};
+
+// 프로젝트 수정
+const patchProject = async (req, res) => {
+    try{
+        await Project.update(
+            {
+            projectName: req.body.projectName,
+            targetDomain: req.body.targetDomain,
+            description: req.body.description
+            },
+            {
+                where: {
+                id: req.params.projectId,
+                user_id: req.id
+                }
+            }
+        );
+        res.status(200).send('프로젝트 수정 성공');
+    } catch(error){
+        console.log(error);
+        res.status(500).send({
+            message: "프로젝트 수정 실패",
+       });
     }
 };
 
@@ -30,7 +55,7 @@ const deleteProject = async (req, res) => {
             }
         });
         if (project){
-            res.json({ ProjectId: req.params.projectId });
+            res.json({ ProjectId: parseInt(req.params.projectId, 10) }); // params는 문자열!
         }
         else{
             res.status(400).send({
@@ -47,5 +72,6 @@ const deleteProject = async (req, res) => {
 
 module.exports = {
     createProject,
+    patchProject,
     deleteProject,
 };
--- src/api/project/project.routes.js
+++ src/api/project/project.routes.js
@@ -6,7 +6,8 @@ const projectController = require("./project.controller");
 
 let routes = (app) => {
     router.post("/create", authJwt, projectController.createProject);
-    router.delete("/delete/:projectId", authJwt, projectController.deleteProject); // DELETE project/delete/1
+    router.patch("/:projectId", authJwt, projectController.patchProject); // PATCH project/1
+    router.delete("/:projectId", authJwt, projectController.deleteProject); // DELETE project/1
 
     app.use("/project/", router);
 };
