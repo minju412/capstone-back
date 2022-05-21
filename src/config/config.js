ce978d28df097f5c37dfbd1445e1e626049fff85
minju412 <ln8847@ajou.ac.kr>
2022-05-22 04:47:22 +0900

    Fix: Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,15 +2,57 @@ require('dotenv').config();
 const env = process.env;
 
 module.exports = {
-    HOST:env.MYSQL_HOST,
-    USER: env.MYSQL_USERNAME,
-    PASSWORD: env.MYSQL_PASSWORD,
-    DB: env.MYSQL_DATABASE,
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+    development : {
+        HOST:env.MYSQL_HOST,
+        USER: env.MYSQL_USERNAME,
+        PASSWORD: env.MYSQL_PASSWORD,
+        DB: env.MYSQL_DATABASE,
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    },
+    test : {
+        HOST:env.MYSQL_HOST,
+        USER: env.MYSQL_USERNAME,
+        PASSWORD: env.MYSQL_PASSWORD,
+        DB: env.MYSQL_DATABASE,
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    },
+    production : {
+        HOST:env.MYSQL_HOST,
+        USER: env.MYSQL_USERNAME,
+        PASSWORD: env.MYSQL_PASSWORD,
+        DB: env.MYSQL_DATABASE,
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    },
 };
+
+// module.exports = {
+//     HOST:env.MYSQL_HOST,
+//     USER: env.MYSQL_USERNAME,
+//     PASSWORD: env.MYSQL_PASSWORD,
+//     DB: env.MYSQL_DATABASE,
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
