b9e4184c60a2714513b63392c47b369d44d782c3
minju412 <ln8847@ajou.ac.kr>
2022-05-24 14:28:53 +0900

    Fix: Use memoryStore with connect-redis

--- src/server.js
+++ src/server.js
@@ -4,8 +4,14 @@ const env = process.env;
 const express = require("express");
 const app = express();
 const morgan = require('morgan'); // morgan은 서버의 콘솔로그를 출력
+const hpp = require('hpp');
+const helmet = require('helmet');
 
-if(process.env.NODE_ENV !== 'test'){ // 테스트 환경에서는 로그없이 깔끔하게 보기 위해서
+if (process.env.NODE_ENV === 'production'){
+    app.use(morgan('combined'));
+    app.use(hpp());
+    app.use(helmet());
+} else{
     app.use(morgan('dev'));
 }
 
@@ -15,7 +21,6 @@ const cors = require('cors');
 const passport = require('passport');
 app.use(passport.initialize());
 require('./middlewares/passport')(passport);
-// const passportConfig = require('./passport/passport.index');
 
 // 라우터 설정
 const initAuthRoutes = require("./api/auth/auth.routes");
@@ -27,9 +32,16 @@ const initProjectRoutes = require("./api/project/project.routes");
 global.__basedir = __dirname + "/..";
 
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const redisClient = require('redis').createClient({url: process.env.REDIS_URL});
+const redisOptions = {
+    client: redisClient,
+    no_ready_check: true,
+    ttl: 600,
+    logErrors: true
+};
+const redisSessionStore = new RedisStore(redisOptions);
 const cookieParser = require('cookie-parser');
-
-// passportConfig();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -45,15 +57,12 @@ app.use(session({
     saveUninitialized: false,
     resave: false,
     secret: env.COOKIE_SECRET,
+    store: redisSessionStore,
     cookie: {
         httpOnly: true,
         secure: false,
     }
 }));
-
-// app.use(passport.initialize()); // 요청 객체에 passport 설정을 심음
-// app.use(passport.session()); // req.session 객체에 passport 정보를 추가 저장
-//// passport.session()이 실행되면, 세션 쿠키 정보를 바탕으로 passport/passport.index.js의 deserializeUser()가 실행된다.
 
 initAuthRoutes(app);
 initCsvRoutes(app);
