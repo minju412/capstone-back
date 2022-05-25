8c5a1e9472c2d42c7dd6d6cf5e94ce4913d9059a
minju412 <ln8847@ajou.ac.kr>
2022-05-25 17:02:51 +0900

    Feat: Add authJwt
    - results.routes에 authJwt 추가

--- src/api/results/results.routes.js
+++ src/api/results/results.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const authJwt = require('../../jwt/authJWT');
 const router = express.Router();
 
 const resultsController = require("./results.controller");
 
 let routes = (app) => {
-    router.get("/", resultsController.getResults);
-    router.get("/number/category", resultsController.categoryCount);
-    router.get("/number/language", resultsController.languageCount);
+    router.get("/", authJwt, resultsController.getResults);
+    router.get("/number/category", authJwt, resultsController.categoryCount);
+    router.get("/number/language", authJwt, resultsController.languageCount);
 
     app.use("/results/", router);
 };
