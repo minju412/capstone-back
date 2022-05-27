1a713dc2651b742a8c90bc16c4ad95fc8afde33b
minju412 <ln8847@ajou.ac.kr>
2022-05-27 18:58:33 +0900

    Fix: Update redis.js

--- src/jwt/redis.js
+++ src/jwt/redis.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const env = process.env;
-// const redis = require('redis');
-// const redisClient = redis.createClient({url: env.REDIS_URL});
+const redis = require('redis');
+const redisClient = redis.createClient({url: env.REDIS_URL});
 
-const Redis = require('ioredis');
-const redisClient = new Redis({
-    port: env.REDIS_PORT,
-    host: env.REDIS_HOST,
-    password: env.REDIS_PASSWORD,
-    db: 0
-})
+// const Redis = require('ioredis');
+// const redisClient = new Redis({
+//     port: env.REDIS_PORT,
+//     host: env.REDIS_HOST,
+//     password: env.REDIS_PASSWORD,
+//     db: 0
+// })
 
 
 //// redis v4
